d7255f46726a25ba2ef326ee222e7244e5acd308
Riley Sumner <rsumner@wisc.edu>
2020-10-05 23:19:24 -0500

    deploy

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import navLinks from '../../../data/navlinks';
 
 @Component({
@@ -8,8 +8,25 @@ import navLinks from '../../../data/navlinks';
 })
 export class NavBarComponent implements OnInit {
   navLinks: any;
-  constructor() {
+  constructor(private el: ElementRef) {
     this.navLinks = navLinks;
+  }
+
+  toggleMenu() {
+    let menu = this.el.nativeElement.querySelector(".nav-items");
+    if(!menu.classList.contains('closed')) {
+      menu.classList.add('closed');
+      menu.style.height = "0px";
+    } else {
+      menu.classList.remove('closed');
+      menu.style.height = (this.navLinks.length * 2.5)+"rem";
+    }
+  }
+
+  closeMenu() {
+    let menu = this.el.nativeElement.querySelector(".nav-items");
+    menu.classList.add('closed');
+    menu.style.height = "0px";
   }
 
   ngOnInit(): void {
