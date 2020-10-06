e2fe57855ddc9e0a4ef8fd74e3080f10c0876cfc
Riley Sumner <rsumner@wisc.edu>
2020-10-05 19:35:27 -0500

    deploy

--- src/data/experience.js
+++ src/data/experience.js
@@ -14,16 +14,16 @@ let experience = [
     title: 'Full Stack Web Developer',
     company: 'UDisc',
     location: 'Remote',
-    start: 'September, 2020',
+    start: 'September 2020',
     end: 'Present',
     desc: 'Implemented site functionality to communication platform viewed by >30,000 users.',
-    lang: ['ReactJS', 'MongoDB', 'HTML', 'CSS', 'MaterialUI']
+    lang: ['ReactJS', 'MongoDB', 'HTML', 'CSS']
   },
   {
     title: 'Front-end Web Developer',
     company: 'UW-Madison Office of Human Resources',
     location: 'Madison, WI',
-    start: 'September, 2018',
+    start: 'September 2018',
     end: 'Present',
     desc: 'Successfully drove the development of online platforms used by staff, jobseekers and students to aid day-to-day operations.',
     lang: ['PHP', 'JavaScript', 'VueJS', 'HTML', 'SCSS', 'jQuery', 'WordPress']
@@ -32,17 +32,26 @@ let experience = [
     title: 'Opinion Desk Editor',
     company: 'UW-Madison Daily Cardinal',
     location: 'Madison, WI',
-    start: 'May, 2020',
+    start: 'May 2020',
     end: 'Present',
     desc: 'Wrote and edited opinion pieces to be published in UW Madison’s campus-wide newspaper.',
-    lang: [],
+    lang: ['Adobe InDesign', 'Adobe Photoshop', 'Adobe Illustrator']
+  },
+  {
+    title: 'Editor',
+    company: 'Bell Magazine',
+    location: 'Madison, WI',
+    start: 'September 2019',
+    end: 'Present',
+    desc: 'Wrote and edited articles published for Bell Magazine, a campus-based magazine.',
+    lang: ['Adobe Photoshop', 'Adobe Illustrator']
   },
   {
     title: 'Web Developer Intern',
     company: 'Bare Home',
     location: 'Blaine, MN',
-    start: 'March, 2018',
-    end: 'August, 2018',
+    start: 'March 2018',
+    end: 'August 2018',
     desc: 'Developed eCommerce marketplace for Bare Home and affiliated brands to sell product line.',
     lang: ['HTML', 'CSS', 'JavaScript', 'HandlebarsJS']
   }
--- src/data/projects.js
+++ src/data/projects.js
@@ -13,7 +13,7 @@
 let projects = [
   {
     title: 'Personal Website',
-    affiliation: 'Personal Project, UW-Madison',
+    affiliation: 'Personal Project',
     start: 'October 2020',
     end: 'Present',
     desc: 'Home for my personal website, developed in AngularJS',
@@ -39,7 +39,7 @@ let projects = [
     desc: 'Developed a web application that teaches students JavaScript. The application features a series of lessons, an environment to test code and an interactive game for users to test their skills.',
     lang: ['Java', 'JavaScript', 'SQL', 'HTML', 'CSS'],
     location: 'Anoka, MN',
-    url: '',
+    url: 'https://github.com/rileyjsumner/Moo-Moo-Code',
   },
   {
     title: 'Travel Application',
@@ -49,8 +49,48 @@ let projects = [
     desc: 'Completed a web application that allows users to plan their vacations and see available amenities around their destination. This application searches real-time flights, hotels, restaurants, entertainment and parks using Google APIs.',
     lang: ['Java', 'JavaScript', 'SQL', 'HTML', 'CSS'],
     location: 'Anoka, MN',
-    url: '',
+    url: 'https://github.com/rileyjsumner/Moo-Moo-Travel',
   },
+  {
+    title: 'Math Tutorial',
+    affiliation: 'BPA, Anoka High School',
+    start: 'September 2015',
+    end: 'February 2016',
+    desc: 'Developed a web application that teaches young learners elementary math concepts by dynamically generating questions for students to answer.',
+    lang: ['Java', 'HTML', 'CSS'],
+    location: 'Anoka, MN',
+    url: 'https://github.com/rileyjsumner/Moo-Moo-Math'
+  },
+  {
+    title: 'D&D Character Builder',
+    affiliation: 'Personal Project',
+    start: 'June 2020',
+    end: 'Present',
+    desc: 'Created a web application that takes users through building a character for the game \'Dungeons & Dragons\'',
+    lang: ['AngularJS', 'JavaScript', 'HTML', 'SCSS'],
+    location: 'Anoka, MN',
+    url: 'https://github.com/rileyjsumner/DND-Character-Builder'
+  },
+  {
+    title: 'Dogwood DIY',
+    affiliation: 'BPA, Anoka High School',
+    start: 'December 2017',
+    end: 'February 2018',
+    desc: 'Created a CRUD based web application for users to share DIY projects on a social media site',
+    lang: ['PHP', 'JavaScript', 'HTML', 'CSS'],
+    location: 'Anoka, MN',
+    url: 'https://github.com/rileyjsumner/BPA-DIY-2017'
+  },
+  {
+    title: 'BPA Cares',
+    affiliation: 'BPA, Anoka High School',
+    start: 'December 2018',
+    end: 'February 2019',
+    desc: 'Developed a web application for the \"BPA Cares\" program to display information to members in a more readable format.',
+    lang: ['ReactJS', 'JavaScript', 'HTML', 'CSS'],
+    location: 'Anoka, MN',
+    url: 'https://github.com/rileyjsumner/BPA-WebDes-2018'
+  }
 ];
 
 export default projects;
