aecbb981fd83b3e2390809623584a6a5ab969e1d
Riley Sumner <rsumner@wisc.edu>
2024-03-28 19:24:29 -0500

    add  character sheet to projects

--- src/static/data/projects.ts
+++ src/static/data/projects.ts
@@ -36,4 +36,14 @@ export const projects = [
     ["react", "typescript", "javascript"],
     "https://rileyjsumner-470dca1084ed.herokuapp.com/"
   ),
+  getProject(
+    "D&D Character Sheet Template",
+    getProjectDate(1, 2024),
+    [
+      "Developed a comprehensive Dungeons & Dragons (D&D) character sheet template using Excel, streamlining the character creation process and enabling users to efficiently build their own characters.",
+      "Implemented automated functionalities within the Excel template to dynamically calculate character stats, abilities, and modifiers based on user inputs, reducing manual calculations and errors.",
+    ],
+    ["excel"],
+    "https://www.etsy.com/listing/1662250427/automated-dd-character-sheet-spreadsheet?click_key=6bd047f93b5594e6c2d217e532f03237e37c8102%3A1662250427&click_sum=e79ac8c8&ref=shop_home_feat_1&pro=1"
+  ),
 ];
